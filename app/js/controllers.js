08bcd9ff9f32898a177e6156c0a5a78de6258deb
unknown <t01141201@0905CD-7DNTSJ1.catdir.com>
2015-03-16 13:26:31 -0400

    updated with custom event handlers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,8 +17,11 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$h
   function ($scope, $routeParams, $http){
     $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data){
         $scope.phone = data; 
+        $scope.mainImageUrl = data.images[0];
     });
       
-      $scope.phoneId = $routeParams.phoneId; 
+      $scope.setImage = function(imageUrl){
+          $scope.mainImageUrl = imageUrl
+      }
 
    }]);
